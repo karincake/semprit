39de9bbe580748ca4281991bb2f0aaac1d57ea1c
Munawwirul Jamal <munawwirul.jamal@gmail.com>
2023-08-30 09:47:50 +0700

    dev: + standardization of varibale names

--- structfiller.go
+++ structfiller.go
@@ -36,40 +36,30 @@ func JsonFromIOReader(container any, input io.Reader) error {
 
 func FDFromHttp(container any, r *http.Request) error {
 	// identiy value and loop if its pointer until reaches non pointer
-	contV := reflect.ValueOf(container)
+	cv := reflect.ValueOf(container)
 
 	// loop until we get what kind lays behind the input
-	for contV.Kind() == reflect.Pointer || contV.Kind() == reflect.Interface {
-		contV = contV.Elem()
+	for cv.Kind() == reflect.Pointer || cv.Kind() == reflect.Interface {
+		cv = cv.Elem()
 	}
 
 	// non struct cant be validated
-	if contV.Kind() != reflect.Struct {
+	if cv.Kind() != reflect.Struct {
 		panic("input requires struct type")
 	}
 
 	// check each field
-	// contT := reflect.TypeOf(contV.Interface()) // keep this for now
-	contT := contV.Type()
-	for i := 0; i < contV.NumField(); i++ {
+	// ct := reflect.TypeOf(cv.Interface()) // keep this for now
+	ct := cv.Type()
+	for i := 0; i < cv.NumField(); i++ {
 		// identify field type and value of the field
-		ft := contT.Field(i)
-		fv := contV.Field(i)
+		ft := ct.Field(i)
+		fv := cv.Field(i)
 		if !fv.CanSet() {
 			continue
 		}
 
-		key := ft.Tag.Get("json")
-		if key != "" {
-			keys := strings.Split(key, ",")
-			if keys[0] != "" {
-				key = keys[0]
-			} else {
-				key = ft.Name
-			}
-		} else {
-			key = ft.Name
-		}
+		key := keyOrJsonTag(ft.Name, ft.Tag.Get("json"))
 
 		fName := ft.Name
 		rv := r.FormValue(key)
@@ -156,57 +146,57 @@ func QPFromHttp(container any, url url.URL) error {
 	cT := cV.Type()
 	values := url.Query()
 	for i := 0; i < cV.NumField(); i++ {
-		fieldV := cV.Field(i)
-		fieldT := cT.Field(i)
+		fv := cV.Field(i)
+		ft := cT.Field(i)
 
-		if !fieldV.CanSet() {
+		if !fv.CanSet() {
 			continue
 		}
 
-		key := keyOrJsonTag(fieldT.Name, fieldT.Tag.Get("json"))
+		key := keyOrJsonTag(ft.Name, ft.Tag.Get("json"))
 
 		vals, ok := values[key]
 		if !ok {
 			continue
 		}
 
-		switch fieldV.Interface().(type) {
+		switch fv.Interface().(type) {
 		case bool, *bool:
 			var v bool
-			fieldVS := strings.ToLower(vals[0])
-			if fieldVS == "true" || fieldVS == "yes" || fieldVS == "1" {
+			fvS := strings.ToLower(vals[0])
+			if fvS == "true" || fvS == "yes" || fvS == "1" {
 				v = true
-			} else if fieldVS == "false" || fieldVS == "no" || fieldVS == "0" {
+			} else if fvS == "false" || fvS == "no" || fvS == "0" {
 				v = false
 			} else {
 				return te.XError{Code: "bool-parse-fail", Message: "failed to parse bool value into field " + key}
 			}
-			if fieldV.Kind() == reflect.Ptr {
-				fieldV.Set(reflect.ValueOf(&v))
+			if fv.Kind() == reflect.Ptr {
+				fv.Set(reflect.ValueOf(&v))
 			} else {
-				fieldV.Set(reflect.ValueOf(v))
+				fv.Set(reflect.ValueOf(v))
 			}
 		case string, *string:
-			if fieldV.Kind() == reflect.Ptr {
-				fieldV.Set(reflect.ValueOf(&vals[0]))
+			if fv.Kind() == reflect.Ptr {
+				fv.Set(reflect.ValueOf(&vals[0]))
 			} else {
-				fieldV.Set(reflect.ValueOf(vals[0]))
+				fv.Set(reflect.ValueOf(vals[0]))
 			}
 		case int, int8, int16, int32, int64, uint, uint8, uint16, uint32, uint64, *int, *int8, *int16, *int32, *int64, *uint, *uint8, *uint16, *uint32, *uint64:
 			if valInt, err := strconv.Atoi(vals[0]); err != nil {
 				return te.XError{Code: "int-parse-fail", Message: "failed to parse int value into field " + key}
 			} else {
-				fieldV.Set(intToVal(valInt, fieldV))
+				fv.Set(intToVal(valInt, fv))
 			}
 		case float64, *float64:
 			strFloat, err := strconv.ParseFloat(vals[0], 64)
 			if err != nil {
 				return te.XError{Code: "float32-parse-fail", Message: "failed to parse float32 value into field " + key}
 			}
-			if fieldV.Kind() == reflect.Ptr {
-				fieldV.Set(reflect.ValueOf(&strFloat))
+			if fv.Kind() == reflect.Ptr {
+				fv.Set(reflect.ValueOf(&strFloat))
 			} else {
-				fieldV.Set(reflect.ValueOf(strFloat))
+				fv.Set(reflect.ValueOf(strFloat))
 			}
 		case float32, *float32:
 			strFloat, err := strconv.ParseFloat(vals[0], 32)
@@ -214,33 +204,33 @@ func QPFromHttp(container any, url url.URL) error {
 				return te.XError{Code: "float64-parse-fail", Message: "failed to parse float64 value into field " + key}
 			}
 			strFloat32 := float32(strFloat)
-			if fieldV.Kind() == reflect.Ptr {
-				fieldV.Set(reflect.ValueOf(&strFloat32))
+			if fv.Kind() == reflect.Ptr {
+				fv.Set(reflect.ValueOf(&strFloat32))
 			} else {
-				fieldV.Set(reflect.ValueOf(strFloat32))
+				fv.Set(reflect.ValueOf(strFloat32))
 			}
 		case []string, *[]string:
-			fieldV.Set(reflect.ValueOf(&vals))
+			fv.Set(reflect.ValueOf(&vals))
 		case datatypes.Date, *datatypes.Date:
 			time, err := time.Parse("2006-01-02T15:04:05.000Z", vals[0])
 			if err != nil {
 				return te.XError{Code: "gormDate-parse-fail", Message: "failed to gorm-date value into field " + key}
 			}
 			date := datatypes.Date(time)
-			if fieldV.Kind() == reflect.Ptr {
-				fieldV.Set(reflect.ValueOf(&date))
+			if fv.Kind() == reflect.Ptr {
+				fv.Set(reflect.ValueOf(&date))
 			} else {
-				fieldV.Set(reflect.ValueOf(date))
+				fv.Set(reflect.ValueOf(date))
 			}
 		case time.Time, *time.Time:
 			time, err := time.Parse("2006-01-02T15:04:05.000Z", vals[0])
 			if err != nil {
 				return te.XError{Code: "time-parse-fail", Message: "failed to time value into field " + key}
 			}
-			if fieldV.Kind() == reflect.Ptr {
-				fieldV.Set(reflect.ValueOf(&time))
+			if fv.Kind() == reflect.Ptr {
+				fv.Set(reflect.ValueOf(&time))
 			} else {
-				fieldV.Set(reflect.ValueOf(time))
+				fv.Set(reflect.ValueOf(time))
 			}
 		// TODO: make any *[]int as a function
 		case *[]int8:
@@ -255,7 +245,7 @@ func QPFromHttp(container any, url url.URL) error {
 				}
 			}
 			if !failed {
-				fieldV.Set(reflect.ValueOf(valX))
+				fv.Set(reflect.ValueOf(valX))
 			}
 
 		}
