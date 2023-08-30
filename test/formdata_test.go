edb1535d62f9e4a59de7131c6f6980e29db89d3d
Munawwirul Jamal <munawwirul.jamal@gmail.com>
2023-08-30 16:25:06 +0700

    dev: + separate uint from int parsing + renamed some func names into more readable + more test data

--- test/formdata_test.go
+++ test/formdata_test.go
@@ -1,6 +1,7 @@
 package test
 
 import (
+	"fmt"
 	"net/http"
 	"net/url"
 	"testing"
@@ -8,44 +9,39 @@ import (
 	s "github.com/karincake/semprit"
 )
 
-func TestFormDataOnSmallString(t *testing.T) {
+func TestMediumDataNormal(t *testing.T) {
 	// mock request
 	r, _ := http.NewRequest("POST", "/", nil)
 	r.PostForm = make(url.Values)
 	r.PostForm.Add("name", "Santo Sembodo")
-
-	// data and wanted
-	data := DataSmallString{}
-	want := DataSmallString{
-		Name: "Santo Sembodo",
-	}
-
-	if err := s.FormDataFromHttp(&data, r); err != nil {
-		t.Error("failed to parse request: ", err)
-	} else {
-		if data != want {
-			t.Error("failed to parse")
-		}
-	}
-}
-
-func TestFormDataOnSmallBool(t *testing.T) {
-	// mock request
-	r, _ := http.NewRequest("POST", "/", nil)
-	r.PostForm = make(url.Values)
 	r.PostForm.Add("married", "true")
+	r.PostForm.Add("score", "20")
+	r.PostForm.Add("creditScore", "-10")
+	r.PostForm.Add("age", "80")
+	r.PostForm.Add("hoursActive", "181500")
+	r.PostForm.Add("income", "800")
+	r.PostForm.Add("netWorth", "200000")
 
 	// data and wanted
-	data := DataSmallBool{}
-	want := DataSmallBool{
-		Married: true,
+	data := DataMedium{}
+	want := DataMedium{
+		Name:        "Santo Sembodo",
+		Married:     true,
+		Score:       20,
+		CreditScore: -10,
+		Age:         80,
+		HoursActive: 181500,
+		Income:      800,
+		NetWorth:    200000,
 	}
 
-	if err := s.FormDataFromHttp(&data, r); err != nil {
+	if err := s.HttpFormData(&data, r); err != nil {
 		t.Error("failed to parse request: ", err)
 	} else {
 		if data != want {
-			t.Error("failed to parse")
+			fmt.Println(data)
+			fmt.Println(want)
+			t.Error("failed to parse ")
 		}
 	}
 }
--- test/type.go
+++ test/type.go
@@ -1,5 +1,31 @@
 package test
 
+type CustomTypeString string
+type CustomTypeBool bool
+type CustomTypeInt8 int8
+type CustomTypeInt int
+type CustomTypeUint8 uint8
+type CustomTypeUint uint
+type CustomTypeFloat32 float32
+type CustomTypeFloat64 float64
+
+const CTSValid CustomTypeString = "valid"
+const CTSInvalid CustomTypeString = "Invalid"
+const CTBSuccess CustomTypeBool = true
+const CTBFail CustomTypeBool = false
+const CTI8First CustomTypeInt8 = 1
+const CTI8Second CustomTypeInt8 = 2
+const CTIFirst CustomTypeInt = 1
+const CTISecond CustomTypeInt = 2
+const CTU8First CustomTypeUint8 = 1
+const CTU8Second CustomTypeUint8 = 2
+const CTUFirst CustomTypeUint = 1
+const CTUSecond CustomTypeUint = 2
+const CTFFirst CustomTypeFloat32 = 1
+const CTFSecond CustomTypeFloat32 = 2
+const CTF6First CustomTypeFloat64 = 1
+const CTF6Second CustomTypeFloat64 = 2
+
 type DataSmallString struct {
 	Name string `json:"name"`
 }
@@ -32,13 +58,56 @@ type DataSmallFloat64 struct {
 	NetWorth float64 `json:"netWorth"`
 }
 
+type DataSmallCustomTypeString struct {
+	Name CustomTypeString `json:"name"`
+}
+
+type DataSmallCustomTypeBool struct {
+	Married bool `json:"married"`
+}
+
+type DataSmallCustomTypeInt8 struct {
+	Score int64 `json:"score"`
+}
+
+type DataSmallCustomTypeInt struct {
+	CreditScore int `json:"creditScore"`
+}
+
+type DataSmallCustomTypeUint8 struct {
+	Age uint8 `json:"age"`
+}
+
+type DataSmallCustomTypeUint struct {
+	HoursActive int `json:"hoursActive"`
+}
+
+type DataSmallCustomTypeFloat32 struct {
+	Income float32 `json:"income"`
+}
+
+type DataSmallCustomTypeFloat64 struct {
+	NetWorth float64 `json:"netWorth"`
+}
+
 type DataMedium struct {
 	Name        string  `json:"name"`
 	Married     bool    `json:"married"`
-	Score       int64   `json:"score"`
+	Score       int8    `json:"score"`
 	CreditScore int     `json:"creditScore"`
 	Age         uint8   `json:"age"`
-	HoursActive int     `json:"hoursActive"`
+	HoursActive uint    `json:"hoursActive"`
 	Income      float32 `json:"income"`
 	NetWorth    float64 `json:"netWorth"`
+}
+
+type DataMediumCT struct {
+	Name         CustomTypeString  `json:"name"`
+	Married      CustomTypeBool    `json:"married"`
+	Score        CustomTypeInt8    `json:"score"`
+	CreditScore  CustomTypeInt     `json:"creditScore"`
+	Age          CustomTypeUint8   `json:"age"`
+	HoursActive  CustomTypeUint    `json:"hoursActive"`
+	IncomeRate   CustomTypeFloat32 `json:"income"`
+	NetWorthRate CustomTypeFloat64 `json:"netWorth"`
 }
